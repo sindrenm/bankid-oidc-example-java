5ccbf70eb83107f15828eec3bf9253fdcff6690b
Kristoffer Skaret <kristofferskaret@kristoffers-mbp.kantega.lan>
2017-01-20 13:15:50 +0100

    Hello World som kjører med mvn jetty:run

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+/**
+ * Created by kristofferskaret on 20.01.2017.
+ */
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
+public class HelloWorldServlet extends HttpServlet
+{
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException
+    {
+        response.getOutputStream().print("Hello World");
+    }
+}
