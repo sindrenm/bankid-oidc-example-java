342f18bd71e92b0f97fa63e71b4c698f4fc2f080
Kristoffer Skaret <kristoffer.skaret@kantega.no>
2017-01-23 14:21:30 +0100

    CallbackServlet som håndterer callback.

---
+++ src/main/java/no/bankid/oidc/web/CallBackServlet.java
@@ -0,0 +1,29 @@
+package no.bankid.oidc.web; /**
+ * Created by kristofferskaret on 20.01.2017.
+ */
+
+import no.bankid.oidc.service.BankIdOauthClient;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/callback"}, loadOnStartup = 1)
+public class CallBackServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        StringBuilder responseHtml = new StringBuilder();
+
+        responseHtml.append("<html>")
+                .append("<head></head><body>")
+                .append("<h1>Callback</h1>")
+                .append("<p>A callback has been made! With a code.</p>")
+                .append("</body></html");
+
+        response.getOutputStream().print(responseHtml.toString());
+    }
+}
