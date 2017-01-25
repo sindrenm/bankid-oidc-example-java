9f49cf8977c34f7ada8e6f4f798d000002ae5932
oyvgru <oyvind.grutle@kantega.no>
2017-01-25 17:00:52 +0100

    Removed password and client id.

--- src/main/java/no/bankid/oidc/ClientDefinition.java
+++ src/main/java/no/bankid/oidc/ClientDefinition.java
@@ -6,6 +6,6 @@ public class ClientDefinition {
      * client_id og passord må legges inn her for at applikasjonen skal fungere.
      * Ta kontakt med BankID Norge (developer@kantega.no) for å ta i bruk eksempelet.
      */
-    public static final String CLIENT_ID = "JavaClient";
-    public static final String CLIENT_PWD = "8765";
+    public static final String CLIENT_ID = "<insert client_id>";
+    public static final String CLIENT_PWD = "<insert client password>";
 }
